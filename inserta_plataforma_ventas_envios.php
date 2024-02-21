31f8e8616aeb6a54896cf23f9584aa065ead8f15
fragolan01 <157424555+fragolan01@users.noreply.github.com>
2024-02-21 11:56:51 -0600

    tabla envios
    Actualiza pero no inserta costo

--- inserta_plataforma_ventas_envios.php
+++ inserta_plataforma_ventas_envios.php
@@ -1,6 +1,6 @@
 <?
 $sql_plataforma_ventas_envios_master_inserta = 
-"INSERT INTO plataforma_ventas_envios(id_dominio,envio) ".
-"VALUES ('$id_dominio','$envio')";
+"INSERT INTO plataforma_ventas_envios(id_dominio,envio,costo) ".
+"VALUES ('$id_dominio','$envio','$costo')";
 $result_plataforma_ventas_envios_master_inserta = mysql_query($sql_plataforma_ventas_envios_master_inserta);
 ?>
--- plataforma_ventas_envios.php
+++ plataforma_ventas_envios.php
@@ -1,5 +1,14 @@
 <?
 
+
+// Muestra todos los errores excepto los de nivel de advertencia
+error_reporting(E_ALL & ~E_WARNING);
+
+// Mostrar los errores en el navegador
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $id_dominio=9999;
 
 if (!$v7) {
@@ -18,13 +27,17 @@ if ($v7=="despliega") {
 	while ($line_plataforma_ventas_envios = mysql_fetch_assoc($result_plataforma_ventas_envios)) {
 		$data_plataforma_ventas_envios0=$line_plataforma_ventas_envios["id"];
 		$data_plataforma_ventas_envios3=$line_plataforma_ventas_envios["envio"];
+		$data_plataforma_ventas_envios4=$line_plataforma_ventas_envios["costo"];
+
 
 		echo 
 			"<nobr>".
 				$data_plataforma_ventas_envios0.".- (<a href='index.php?v7=actualizar&v13=".
 				$data_plataforma_ventas_envios0."'>Actualizar</a> | <a href='index.php?v7=eliminar&v13=".
 				$data_plataforma_ventas_envios0."'>Eliminar</a>) Envio ".
-				$data_plataforma_ventas_envios3.
+				$data_plataforma_ventas_envios3.  " - Costo: " .
+				$data_plataforma_ventas_envios4.
+
 			"</nobr>";
 		
 		echo "<br>";
@@ -38,15 +51,20 @@ if ($v7=="actualizar") {
 		echo "<b>ACTUALIZAR:</b>";
 		echo "<br><br>";
 
-		$query_plataforma_ventas_envios = "SELECT id,id_dominio,envio FROM plataforma_ventas_envios WHERE id='".$v13."' AND id_dominio='".$id_dominio."'";
+		$query_plataforma_ventas_envios = "SELECT id,id_dominio,envio,costo FROM plataforma_ventas_envios WHERE id='".$v13."' AND id_dominio='".$id_dominio."'";
 		$result_plataforma_ventas_envios = mysql_query($query_plataforma_ventas_envios) or die('Query failed: plataforma_ventas_envios ' . mysql_error());
 		while ($line_plataforma_ventas_envios = mysql_fetch_assoc($result_plataforma_ventas_envios)) {
 			$data_plataforma_ventas_envios0=$line_plataforma_ventas_envios["id"];
 			$data_plataforma_ventas_envios3=$line_plataforma_ventas_envios["envio"];
+			$data_plataforma_ventas_envios4=$line_plataforma_ventas_envios["costo"];
 		}
 
 		echo "<form name='actualiza_plataforma_ventas_envios' method='post' action='index.php?v7=actualizalo&v13=".$v13."'>";
-			echo "<nobr>Envio <input type='text' name='envio' value='".$data_plataforma_ventas_envios3."' size='50'><input type='submit' name='actualizar' value='ACTUALIZAR &raquo;'></nobr>";
+			echo "<nobr>
+					Envio <input type='text' name='envio' value='".$data_plataforma_ventas_envios3."' size='50'>
+					Costo <input type='text' name='costo' value='".$data_plataforma_ventas_envios4."' size='50'>
+					<input type='submit' name='actualizar' value='ACTUALIZAR &raquo;'>
+				</nobr>";
 		echo "</form>";
 	}
 	else {
@@ -55,24 +73,31 @@ if ($v7=="actualizar") {
 		echo "<br><br>";
 
 		echo "<form name='inserta_plataforma_ventas_envios' method='post' action='index.php?v7=actualizalo'>";
-			echo "<nobr>Envio: <input type='text' name='envio' value='' size='50'><input type='submit' name='insertar' value='INSERTAR &raquo;'></nobr>";
+			echo "<nobr>
+					Envio: <input type='text' name='envio' value='' size='50'>
+					Costo: <input type='text' name='costo' value='' size='50'>
+					<input type='submit' name='insertar' value='INSERTAR &raquo;'>
+				</nobr>";
 		echo "</form>";
 	}
 }
 if ($v7=="actualizalo") {
 	if ($v13) {
+
 		echo "<br><br>";
 		echo "<b>UPDATE:</b>";
 		echo "<br><br>";
 
+
+		$costo=$_POST['costo'];
 		$envio=$_POST["envio"];
-		if (!$envio) {
-			echo "<font color=red>ERROR! Falta el Envio...</font>";
+		if (!$envio and !$costo) {
+			echo "<font color=red>ERROR! Falta el Envio y Costo...</font>";
 		}
 		else {
-			$sql_plataforma_ventas_envios = "UPDATE plataforma_ventas_envios SET envio='".$envio."' WHERE id='".$v13."' AND id_dominio='".$id_dominio."'";
+			// $sql_plataforma_ventas_envios = "UPDATE plataforma_ventas_envios SET envio='".$envio."' WHERE id='".$v13."' AND id_dominio='".$id_dominio."'";
+			$sql_plataforma_ventas_envios = "UPDATE plataforma_ventas_envios SET envio='".$envio."', costo='".$costo."' WHERE id='".$v13."' AND id_dominio='".$id_dominio."'";
 			$result_plataforma_ventas_envios= mysql_query($sql_plataforma_ventas_envios);
-
 			echo "<font color=blue>PERFECTO! Envio actualizado...</font>";
 
 			echo $recargador;
@@ -83,16 +108,18 @@ if ($v7=="actualizalo") {
 		echo "<b>INSERT:</b>";
 		echo "<br><br>";
 
+		$costo=$_POST['costo'];
 		$envio=$_POST["envio"];
-		if (!$envio) {
-			echo "<font color=red>ERROR! Falta el Envio...</font>";
+		if (!$envio and !$costo) {
+			echo "<font color=red>ERROR! Falta el Envio o Costo...</font>";
 		}
 		else {
 			$id_dominio=$id_dominio;
 			$envio=$envio;
+			$costo=$costo;
 			require($laraiz."inserta_plataforma_ventas_envios.php");
 
-			echo "<font color=green>PERFECTO! Envioi insertado...</font>";
+			echo "<font color=green>PERFECTO! Envio insertado...</font>";
 
 			echo $recargador;
 		}
