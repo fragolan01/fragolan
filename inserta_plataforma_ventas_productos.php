7c3a25d56f14dfccb13343969a250121cf886ce8
fragolan01 <157424555+fragolan01@users.noreply.github.com>
2024-02-22 11:56:17 -0600

    Producto/precio-vta
    Producto/precio-vta terminado:
    Inserta
    Actuliza
    Elimina

--- inserta_plataforma_ventas_productos.php
+++ inserta_plataforma_ventas_productos.php
@@ -1,6 +1,6 @@
 <?
-    $sql_plataforma_ventas_productos_master_inserta = "INSERT INTO plataforma_ventas_productos(id_dominio, producto, modelo, num_piezas, inventario_minimo) "."VALUES
-    ('$id_dominio','$producto', '$modelo', '$num_piezas', '$inventario_minimo')";
+    $sql_plataforma_ventas_productos_master_inserta = "INSERT INTO plataforma_ventas_productos(id_dominio, producto, modelo, num_piezas, inventario_minimo, precio_venta) "."VALUES
+    ('$id_dominio','$producto', '$modelo', '$num_piezas', '$inventario_minimo', '$precio_venta')";
     $result_plataforma_ventas_productos_master_inserta = mysql_query($sql_plataforma_ventas_productos_master_inserta);
 
 
